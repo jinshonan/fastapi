562879534ee75de053eb3611bf3e7e8faf86f77b
Jin Shonan <jeffrey.zsn1126@gmail.com>
2025-05-16 23:23:34 +0900

    authentication complete

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-
-from .. import database, schemas, models, utils
+from .. import database, schemas, models, utils, oauth2
 
 # this file is used for user password authentication
 
@@ -9,12 +9,13 @@ router = APIRouter(
     tags=["Authentication"]
 )
 
-@router.post("/login")
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.get_db)):
-    user = db.query(models.User).filter(models.User.email==user_credentials.email).first()
+@router.post("/login", response_model=schemas.Token)  # submit user name and password in Body: form-data
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.email==user_credentials.username).first()
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Email")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Email")
     if not utils.verify(user_credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Password")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Password")
     # create a token and return it
-    return {"message": "login successful"}
+    access_token = oauth2.create_access_token(data = {"user_id": user.id})
+    return {"access_token": access_token, "token_type": "bearer"}
--- app/routers/post.py
+++ app/routers/post.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from typing import List
-from .. import models, schemas, utils
+from .. import models, schemas, utils, oauth2
 from .. database import get_db
 
 router = APIRouter(
@@ -11,13 +11,16 @@ router = APIRouter(
 
 
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     posts = db.query(models.Post).all()
     return posts
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
+def create_posts(post: schemas.PostCreate, 
+                 db: Session = Depends(get_db), 
+                 current_user: int = Depends(oauth2.get_current_user)):  # authorization in headers
+    print(current_user.email)
     new_post = models.Post(**post.model_dump())
     db.add(new_post)
     db.commit()
@@ -26,14 +29,15 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
 
 
 @router.get("/{id}", response_model=schemas.Post)
-def get_post(id: int, db: Session = Depends(get_db)):
+def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == id).first()  # find the first instance that's a match
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"id {id} doesn't exist")
     return post
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
+def delete_post(id: int, db: Session = Depends(get_db), 
+                user_id: int = Depends(oauth2.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == id)
     if post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"{id} not found")
@@ -42,7 +46,9 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @router.put("/{id}", response_model=schemas.Post)
-def delete_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
+def update_post(id: int, post: schemas.PostCreate, 
+                db: Session = Depends(get_db),
+                current_user: int = Depends(oauth2.get_current_user)):
     post_query = db.query(models.Post).filter(models.Post.id == id)
     if post_query.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"id {id} doesn't exist")
